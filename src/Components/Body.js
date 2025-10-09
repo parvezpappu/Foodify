66d180d5db7b2a066127e48f5bcec1326cecf682
Parvez Pappu <mdparveahmedpappu@gmail.com>
2025-10-10 01:27:48 +0600

    Button : Filtered and Search Button added

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,32 +5,36 @@
 
   const Body=()=>{
   const [searchText,setSearchText]=useState("");
-  const  [filteredRestaurantInfo,RestaurantInfo,setRestaurantInfo,setfileredRestaurantInfo]=useRestaurantCart();
-    
-
-  
-
-
+  const [filteredRestaurantInfo,RestaurantInfo,setRestaurantInfo,setfileredRestaurantInfo]=useRestaurantCart();
+ 
     if(RestaurantInfo.length===0){
       return (<Shimmer/>)
     }
-
     return (
      <>
+      <div className="flex">
         <div className="input ">
          <input className ="border-[2px] rounded-[15px] p-[5px]" type="text" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
         </div>
-        <button className="button" onClick={()=>{
+        <button className="button border-[2px] p-[5px] rounded-[15px] ml-[7px] cursor-pointer hover:bg-blue-900 " onClick={()=>{
          const filteredData=RestaurantInfo.filter((value)=>value.info.name.toLowerCase().includes(searchText.toLowerCase()));
-         setfileredRestaurantInfo(filteredData)
+         setfileredRestaurantInfo(filteredData);
         }}>Search</button>
+      
+          <button className="border-[2px] p-[5px] rounded-[15px] ml-[7px] cursor-pointer hover:bg-blue-900" onClick={()=>{
+              const filteredTopRatedRestaurant=RestaurantInfo.filter((value)=> value.info.avgRatingString>=4.5);
+               setfileredRestaurantInfo(filteredTopRatedRestaurant);
+               setRestaurantInfo(filteredTopRatedRestaurant);
+          }}>Top Rated Restaurant</button>
+         
+      
+          </div>
 
-        <div className="flex flex-wrap justify-evenly gap-6 p-4">
+        <div className="grid gap-6 grid-cols-[repeat(auto-fill,minmax(260px,1fr))] mt-[20px] ml-[10px]">
           {
              filteredRestaurantInfo.map((value)=>(
               < RestuaratnCart key={value.info.id} resData={value.info} />
-             ))
-            
+             )) 
           }
         </div>
         </>
--- src/Components/ShimerUi.js
+++ src/Components/ShimerUi.js
@@ -2,7 +2,10 @@
 
  const Shimmer=()=>{
     return (
-        <div className="shimmer flex flex-wrap justify-evenly ml-[20px]">
+       <div>
+
+       <h1 className="m-[20px]">Loading...</h1>
+        <div className="shimmer flex flex-wrap justify-evenly ml-[20px]">     
               <div className="w-[200px] h-[300px] bg-gray-700 m-[5px] "></div>
               <div className="w-[200px] h-[300px] bg-gray-700 m-[5px] "></div>
               <div className="w-[200px] h-[300px] bg-gray-700 m-[5px] "></div>
@@ -28,9 +31,9 @@
               <div className="w-[200px] h-[300px] bg-gray-700 m-[5px] "></div>
               <div className="w-[200px] h-[300px] bg-gray-700 m-[5px] "></div>
            
-                 
-        
-        </div>
+              </div>   
+               </div>
+       
     )
  }
 
